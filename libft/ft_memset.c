18bc5a350c646c438fc50376208f5d416b0e4959
malatini <malatini@student.42.fr>
2023-10-03 15:38:15 +0200

    fix norminette

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mahautlatinis <mahautlatinis@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 23:11:04 by malatini          #+#    #+#             */
-/*   Updated: 2023/10/03 15:30:32 by mahautlatin      ###   ########.fr       */
+/*   Updated: 2023/10/03 15:37:56 by mahautlatin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memset(void *dest, int ch, size_t count)
 {
-	unsigned char *bytes;
+	unsigned char	*bytes;
 
 	bytes = (unsigned char *)dest;
 	while (count)
