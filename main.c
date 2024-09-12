43af7bd529c968b8cfe7305ac2e3a2647bb284bf
mahautlatinis <mahautlatinisheritier@gmail.com>
2024-09-12 11:26:42 +0200

    remove main file

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "./libft/include/libft.h"
-#include <ctype.h>
-
-int main(void)
-{
-	printf("Is alpha is ? %i\n", ft_isalpha('a'));
-	printf("Is alpha is ? %i\n", isalpha('a'));
-}
